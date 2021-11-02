b49c8915e0cdf497d94bbd9cc55190c8b8a57277
AhsanRiaz9 <bcsf18m009@pucit.edu.pk>
2021-11-02 23:20:31 +0500

    Create MC Multiple Screens Practice Android Project

---
+++ app/src/main/java/com/example/mc_multiple_screens_practice_bcsf18m009/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mc_multiple_screens_practice_bcsf18m009;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
