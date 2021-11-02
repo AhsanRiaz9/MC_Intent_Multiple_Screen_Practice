07476478a2008dca269d3a02020d8f3366b3f79c
AhsanRiaz9 <bcsf18m009@pucit.edu.pk>
2021-11-03 00:18:34 +0500

    Added new activity using intent

--- app/src/main/java/com/example/mc_multiple_screens_practice_bcsf18m009/MainActivity.java
+++ app/src/main/java/com/example/mc_multiple_screens_practice_bcsf18m009/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.mc_multiple_screens_practice_bcsf18m009;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button newActivityBtn;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        newActivityBtn = findViewById(R.id.newActivity);
+        newActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
